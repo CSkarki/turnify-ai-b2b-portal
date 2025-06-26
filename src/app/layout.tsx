a23d9d20d40b444e7753062c218e32563fdff083
CSkarki <your.email@example.com>
2025-06-26 14:13:37 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Turnify B2B Return Portal',
+  title: 'Turnify B2B Return Portal  ...',
   description: 'B2B Return Management Portal for Turnify',
 }
 
